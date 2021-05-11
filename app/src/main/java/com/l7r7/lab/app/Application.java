5b2d319ef6d6ad5d5a305f89ed09bc512af91bd5
Leonhard Riedisser <leo008180@googlemail.com>
2021-05-11 11:25:45 +0200

    Show different options for including modules

--- app/src/main/java/com/l7r7/lab/app/Application.java
+++ app/src/main/java/com/l7r7/lab/app/Application.java
@@ -1,10 +1,10 @@
 package com.l7r7.lab.app;
 
 import com.l7r7.lab.module.a.ModuleAConfiguration;
-import com.l7r7.lab.module.b.ModuleBConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -15,15 +15,21 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * Option 1: Configuration class inside the module that does the component scan and is imported here
+     */
     @Configuration
     @Import(ModuleAConfiguration.class)
     @ConditionalOnProperty("module.a.enabled")
     static class ModuleA {
     }
 
+    /**
+     * Option 2: component scan for the package of the module is done directly here
+     */
     @Configuration
     @ConditionalOnProperty("module.b.enabled")
-    @Import(ModuleBConfiguration.class)
+    @ComponentScan("com.l7r7.lab.module.b")
     static class ModuleB {
     }
 }
--- module-b/src/main/java/com/l7r7/lab/module/b/ModuleBConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.l7r7.lab.module.b;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class ModuleBConfiguration {
-}
